22473b2927721c7dbf998140eaef2c63cd782602
Zoltán Werner <39615472+wernerzoltan@users.noreply.github.com>
2020-01-25 16:36:19 +0100

    Add files via upload

--- app/map/map.component.ts
+++ app/map/map.component.ts
@@ -1,5 +1,23 @@
 import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
+import { icon, Marker } from 'leaflet';
+import { MarkerService } from '../_services/marker.service';
+
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
+
 
 @Component({
   selector: 'app-map',
@@ -10,7 +28,7 @@ export class MapComponent implements AfterViewInit {
 
   private map;
 
-  constructor() { }
+  constructor(private markerService: MarkerService) { }
 
   private initMap(): void {
     this.map = L.map('map', {
@@ -29,5 +47,7 @@ export class MapComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     this.initMap();
-  }
+    this.markerService.makeCapitalMarkers(this.map);
+  };
+
 }
